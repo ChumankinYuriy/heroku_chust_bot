469f5b0e45dda310990477311aa88dae0e3741fd
chumankin <chumankinyuriy@mail.ru>
2020-07-03 22:50:53 +0300

    Отлажена и прокомментирована модель переноса стиля.

--- core.py
+++ core.py
@@ -1,4 +1,4 @@
-from time import sleep
+import os
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
@@ -6,13 +6,11 @@ import torch.optim as optim
 from PIL import Image
 import torchvision.transforms as transforms
 import torchvision.models as models
-#from IPython.display import clear_output
+from IPython.display import clear_output
 import random
 
 #Размер к которому будут отмасштабированы картинки.
 imsize = 128
-# TODO: Убрать.
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 # Набор преобразований перед входом на сеть (изменение размера и преобразование из картинки в тензор).
 preprocessor = transforms.Compose([
     transforms.Resize((imsize, imsize)),
@@ -20,9 +18,16 @@ preprocessor = transforms.Compose([
     ])
 # Преобразование из тензора в картинку.
 unloader = transforms.ToPILImage()
-# Предобученная vgg
-#cnn = models.vgg19(pretrained=True).features.to(device).eval()
+vgg_file = 'vgg.cnn'
 
+#cnn = None
+# Загрузка предобученной vgg
+#if os.path.isfile(vgg_file):
+#    cnn = torch.load(vgg_file)
+#else:
+#    cnn = models.vgg19(pretrained=True).features.eval()
+#    torch.save(cnn, vgg_file)
+#cnn.eval()
 
 def load_square_image(path):
     """
@@ -35,7 +40,7 @@ def load_square_image(path):
     image = Image.open(path)
     image = transforms.CenterCrop(min(image.width, image.height))(image)
     image = preprocessor(image).unsqueeze(0)
-    return image.to(device, torch.float)
+    return image
 
 
 def core(content_path: str, style_path: str, tmp_dir='tmp/'):
@@ -50,15 +55,20 @@ def core(content_path: str, style_path: str, tmp_dir='tmp/'):
     :return: Путь и имя файла с результатом переноса стиля.
     """
     content = load_square_image(content_path)
-    res_filename = tmp_dir + str(random.random()) + '.png'
+    res_filename = tmp_dir + str(random.randint(0,0,999999)) + '.png'
     unloader(content.squeeze(0)).save(res_filename)
     return res_filename
 
 
-# Класс функции потерь содержания для простого переноса стиля.
+# Класс функции потерь содержания для переноса стиля.
 class ContentLoss(nn.Module):
 
-    def __init__(self, target,):
+    def __init__(self, target):
+        """
+        Конструктор
+        :param target: FloatTensor
+             Карта признаков на которую должно быть похоже содержание.
+        """
         super(ContentLoss, self).__init__()
         # we 'detach' the target content from the tree used
         # to dynamically compute the gradient: this is a stated value,
@@ -71,55 +81,95 @@ class ContentLoss(nn.Module):
         return input
 
 
+# Класс функции потерь стиля для переноса стиля.
 class StyleLoss(nn.Module):
 
     def gram_matrix(self, input):
-      a, b, c, d = input.size()  # a=batch size(=1)
-      # b=number of feature maps
-      # (c,d)=dimensions of a f. map (N=c*d)
-      features = input.view(a * b, c * d)  # resise F_XL into \hat F_XL
-      G = torch.mm(features, features.t())  # compute the gram product
-      # we 'normalize' the values of the gram matrix
-      # by dividing by the number of element in each feature maps.
-      return G.div(a * b * c * d)
+        """
+        Вычислить матрицу Грама.
+        :param input: FloatTensor
+            Изображение для которого вычисляется матрица.
+        :return: FloatTensor
+            Матрица Грама.
+        """
+        a, b, c, d = input.size()  # a=batch size(=1)
+        # b=number of feature maps
+        # (c,d)=dimensions of a f. map (N=c*d)
+        features = input.view(a * b, c * d)  # resise F_XL into \hat F_XL
+        G = torch.mm(features, features.t())  # compute the gram product
+        # we 'normalize' the values of the gram matrix
+        # by dividing by the number of element in each feature maps.
+        return G.div(a * b * c * d)
 
     def __init__(self, target_feature):
+        """
+        Конструктор.
+        :param target_feature: FloatTesor
+             Карта признаков на которую должен быть стиль.
+        """
         super(StyleLoss, self).__init__()
         self.target = self.gram_matrix(target_feature).detach()
 
     def forward(self, input):
+        """
+        Выполнить расчёт выхода и сохранить значение функции потерь в поле loss.
+        :param input: FloatTensor
+            Вход слоя.
+        :return: FloatTensor
+            Выход слоя, равен входу.
+        """
         G = self.gram_matrix(input)
         self.loss = F.mse_loss(G, self.target)
         return input
 
 
-# create a module to normalize input image so we can easily put it in a
-# nn.Sequential
+# Класс для приведения статистик входного изображения к заданным значениям.
 class Normalization(nn.Module):
-    def __init__(self, mean=torch.tensor([0.485, 0.456, 0.406]).to(device),
-                 std=torch.tensor([0.229, 0.224, 0.225]).to(device)):
+    def __init__(self, mean=torch.tensor([0.485, 0.456, 0.406]),
+                 std=torch.tensor([0.229, 0.224, 0.225])):
+        """
+        Конструктор.
+        :param mean: FloatTensor
+            Тензор средних значений по каналам (каналов должно быть 3).
+        :param std: FloatTensor
+            Тензор стандартных отклонений по каналам (каналов должно быть 3).
+        """
         super(Normalization, self).__init__()
         # .view the mean and std to make them [C x 1 x 1] so that they can
         # directly work with image Tensor of shape [B x C x H x W].
         # B is batch size. C is number of channels. H is height and W is width.
-        self.mean = torch.tensor(mean).view(-1, 1, 1)
-        self.std = torch.tensor(std).view(-1, 1, 1)
+        self.mean = mean.clone().detach().view(-1, 1, 1)
+        self.std = mean.clone().detach().view(-1, 1, 1)
 
     def forward(self, img):
-        # normalize img
+        """
+        Приветси статистики к фиксированным значениям.
+        :param img: FloatTensor
+            Тензор изображения над которым выполняется преобразование.
+        :return: FloatTensor
+            Преобразованный тензор.
+        """
         return (img - self.mean) / self.std
 
 
+# Модель для получения сети, которая рассчитывает функции потерь для переноса стиля.
 class VggFeaturesWithStyleTransferLosses(nn.Module):
     def __init__(self, content_img, style_img):
+        """
+        Конструктор.
+        :param content_img: FloatTensor
+            Содержание.
+        :param style_img: FloatTensor
+            Стиль.
+        """
         super(VggFeaturesWithStyleTransferLosses, self).__init__()
-        cnn = models.vgg19(pretrained=True).features.to(device).eval()
+        cnn = models.vgg11(pretrained=True).features.eval()
         self.layers = nn.Sequential(Normalization())
         self.content_loss_layers = []
         self.style_loss_layers = []
-        i=0
+        i = 0
         for layer in cnn.children():
-            # Копирование в сеть слоёв из vgg
+            # Копирование в сеть слоёв из vgg.
             if isinstance(layer, nn.Conv2d):
                 i += 1
                 name = 'conv_{}'.format(i)
@@ -138,28 +188,32 @@ class VggFeaturesWithStyleTransferLosses(nn.Module):
 
             # Если текущий слой свёрточный, то после него надо добавить loss слои.
             if isinstance(layer, nn.Conv2d):
-                # add content loss:
-                # Чтобы результирующя картинка была не слишком похожа на исходную,
-                # будем брать в контент лосс только выходы последних слоёв.
-                if i==4:
-                  target = self.layers(content_img).detach()
-                  content_loss = ContentLoss(target)
-                  self.layers.add_module("content_loss_{}".format(i), content_loss)
-                  self.content_loss_layers.append(content_loss)
-                # add style loss:
-                target_feature = self.layers(style_img).detach()
-                style_loss = StyleLoss(target_feature)
-                self.layers.add_module("style_loss_{}".format(i), style_loss)
-                self.style_loss_layers.append(style_loss)
+                # Чтобы ускорить вычисления, будем добавлять расчёт функций потерь не после каждого свёрточного слоя.
+                if i==1:
+                    target = self.layers(content_img).detach()
+                    content_loss = ContentLoss(target)
+                    self.layers.add_module("content_loss_{}".format(i), content_loss)
+                    self.content_loss_layers.append(content_loss)
+                if True:
+                    target_feature = self.layers(style_img).detach()
+                    style_loss = StyleLoss(target_feature)
+                    self.layers.add_module("style_loss_{}".format(i), style_loss)
+                    self.style_loss_layers.append(style_loss)
 
-        # now we trim off the layers after the last content and style losses
         for i in range(len(self.layers) - 1, -1, -1):
-            if isinstance(self.layers[i], ContentLoss) or \
-                 isinstance(self.layers[i], StyleLoss):
+            if isinstance(self.layers[i], ContentLoss) or isinstance(self.layers[i], StyleLoss):
                 break
-            self.layers = self.layers[:(i + 1)]
+        self.layers = self.layers[:(i + 1)]
 
     def forward(self, input):
+        """
+        Выполнить прямое распространение по сети
+        с сохранением значений функций потерь в поля content_loss и style_loss.
+        :param input: FloatTensor
+            Вход сети.
+        :return: FloatTensor
+            Выход сети.
+        """
         output = self.layers(input)
         self.style_loss = 0
         for sl in self.style_loss_layers:
@@ -170,37 +224,47 @@ class VggFeaturesWithStyleTransferLosses(nn.Module):
         return output
 
 
-def get_input_optimizer(input_img):
-    # this line to show that input is a parameter that requires a gradient
-    optimizer = optim.LBFGS([input_img.requires_grad_()])
-    return optimizer
-
-
 def style_transfer(model, input_img, num_steps=300,
-                   content_weight=1, style_weight=1000000):
-    optimizer = get_input_optimizer(input_img)
+                   content_weight=1, style_weight=1000000, std_out=True):
+    """
+    Выполнить перенос стиля.
+    :param model: VggFeaturesWithStyleTransferLosses
+        Модель, осуществляющая вычисление значений функций потерь.
+    :param input_img: FloatTensor
+        Буфер для сохранения результата с начальным приближением.
+    :param num_steps: int
+        Количество итераций, которое будет выполнено.
+    :param content_weight: float
+        Вес функции потерь содержания в суммарных потерях.
+    :param style_weight: float
+        Вес функции потерь стиля в суммарных потерях.
+    :param std_out: Boolean
+        Выводить ли лог в стандартный поток вывода.
+    :return: FloatTensor
+        Результат переноса стиля.
+    """
+    optimizer = optim.LBFGS([input_img.requires_grad_()])
     print('Optimizing..')
     cur_step = [0]
-    while cur_step[0] <= num_steps:
-      def closure():
+
+    def closure():
         # correct the values of updated input image
         input_img.data.clamp_(0, 1)
         optimizer.zero_grad()
         model(input_img)
-        loss = content_weight * model.content_loss \
-          + style_weight * model.style_loss
+        loss = content_weight * model.content_loss + style_weight * model.style_loss
         loss.backward()
+        if std_out:
+            print("run {}:".format(cur_step))
+            print('Style Loss : {:4f} Content Loss: {:4f}'.format(
+                model.style_loss.item(), model.content_loss.item()))
         cur_step[0] += 1
-        if cur_step[0] % 50 == 0:
-          #clear_output(wait=True)
-          print("run {}:".format(cur_step))
-          print('Style Loss : {:4f} Content Loss: {:4f}'.format(
-            model.style_loss.item(), model.content_loss.item()))
         return loss
-      optimizer.step(closure)
+
+    while cur_step[0] < num_steps:
+        optimizer.step(closure)
     # a last correction...
     input_img.data.clamp_(0, 1)
     return input_img
 
-#model = VggFeaturesWithStyleTransferLosses(content_img,style_img)
-#output = style_transfer(model, content_img.clone(), 100, 1, 1e+7)
+
---
+++ core_test.py
@@ -0,0 +1,22 @@
+import asyncio
+import random
+from datetime import datetime
+from core import load_square_image, VggFeaturesWithStyleTransferLosses, style_transfer, unloader
+
+
+def main():
+    start_time = datetime.now()
+
+    content_img = load_square_image('styles/4.jpg')
+    style_img = load_square_image('styles/3.jpg')
+    res_filename = 'tmp/' + str(random.randint(0, 999999)) + '.png'
+    model = VggFeaturesWithStyleTransferLosses(content_img, style_img)
+    output = style_transfer(model, content_img.clone(), 100, 1, 1e+6)
+    unloader(output.squeeze(0)).save(res_filename)
+
+    print(datetime.now() - start_time)
+
+
+if __name__ == '__main__':
+    main()
+
